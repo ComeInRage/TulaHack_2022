4dd00f1199d87a3c8bdaa0abeb78261c19ccc78f
ComeInRage <montekek011@mail.ru>
2022-04-23 01:43:41 +0300

    tgbot-cpp submodule moved to TgCore. Created special statuc library for core files

--- main.cpp
+++ main.cpp
@@ -1,27 +1,31 @@
-#include <stdio.h>
+﻿#include <iostream>
 #include <tgbot/tgbot.h>
 
 int main() {
-    TgBot::Bot bot("PLACE YOUR TOKEN HERE");
-    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
-        bot.getApi().sendMessage(message->chat->id, "Hi!");
-    });
-    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
-        printf("User wrote %s\n", message->text.c_str());
-        if (StringTools::startsWith(message->text, "/start")) {
-            return;
-        }
-        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
-    });
-    try {
-        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
-        TgBot::TgLongPoll longPoll(bot);
-        while (true) {
-            printf("Long poll started\n");
-            longPoll.start();
-        }
-    } catch (TgBot::TgException& e) {
-        printf("error: %s\n", e.what());
-    }
+    // Здесь пример использования бота.
+    // Нужно накатить синтаксического сахара и легковесных оберток вокруг этой шляпы
+    // Либо забить и юзать так
+
+    // TgBot::Bot bot("5332685416:AAE8ElGu8XG58WiXfTujjCyk-TtYCgtMl-c");
+    // bot.getEvents().onCommand("start2", [&bot](TgBot::Message::Ptr message) {
+    //     bot.getApi().sendMessage(message->chat->id, "Андрей ololo!");
+    // });
+    // bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+    //     std::cout << "User wrote: " << message->text << std::endl;
+    //     if (StringTools::startsWith(message->text, "/start")) {
+    //         return;
+    //     }
+    //     bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+    // });
+    // try {
+    //     std::cout << "Bot username: " << bot.getApi().getMe()->username << std::endl;
+    //     TgBot::TgLongPoll longPoll(bot);
+    //     while (true) {
+    //         std::cout << "Started event poll" << std::endl;
+    //         longPoll.start();
+    //     }
+    // } catch (TgBot::TgException& e) {
+    //     std::cout << "error: " << e.what() << std::endl;
+    // }
     return 0;
 }
